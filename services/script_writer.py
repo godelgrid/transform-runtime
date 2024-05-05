67e71e58691aae62e3dc73e538e13f8e99f927cd
Mayank Bhargava <mbhargava.gd@gmail.com>
2024-05-05 18:01:48 +0530

    inline script indent correction

--- services/script_writer.py
+++ services/script_writer.py
@@ -1,13 +1,13 @@
 import uuid
 from types import CodeType
 
-INLINE_TRANSFORMER = """
-def transform(self, data):
-    __SCRIPT__
+INLINE_TRANSFORMER = """def transform(self, data):
+__SCRIPT__
 """
 
 
 def compile_script(script: str) -> CodeType:
     script_id: str = str(uuid.uuid4()).replace("-", "")
+    script = '\n'.join(['\t' + line for line in script.splitlines()])
     transformer_script = INLINE_TRANSFORMER.replace("__SCRIPT__", script)
     return compile(transformer_script, f"script{script_id}", 'exec')
