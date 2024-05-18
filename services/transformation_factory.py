4c07c33a05af9e3cd8a61ba7f7fe55f4ad63c36b
Mayank Bhargava <mbhargava.gd@gmail.com>
2024-05-18 20:42:47 +0530

    factory lock

--- services/transformation_factory.py
+++ services/transformation_factory.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from types import CodeType
 from typing import List, Optional, Tuple
 
@@ -11,8 +12,10 @@ class TransformationFactory:
 
     def __init__(self):
         self._transformation_map = dict()
+        self._factory_lock = threading.Lock()
 
     def verify_inline_module(self, script: str) -> [bool, Optional[str]]:
+        # not taking lock as the function is purely computational
         script_object: CodeType
         try:
             script_object = compile_script(script)
@@ -23,93 +26,102 @@ class TransformationFactory:
         return True, None
 
     def load_inline_module(self, transformation_id: str, script: str) -> [bool, str]:
-        if transformation_id in self._transformation_map:
-            transformer = self._transformation_map.pop(transformation_id)
-            del transformer
+        with self._factory_lock:
+            if transformation_id in self._transformation_map:
+                transformation = self._transformation_map.pop(transformation_id)
+                del transformation
 
-        script_object: CodeType
-        try:
-            script_object = compile_script(script)
-        except Exception as e:
-            return False, str(e)
+            script_object: CodeType
+            try:
+                script_object = compile_script(script)
+            except Exception as e:
+                return False, str(e)
 
-        self._transformation_map[transformation_id] = (script_object, EXECUTABLE_TYPE_INLINE,)
-        return True, None
+            self._transformation_map[transformation_id] = (script_object, EXECUTABLE_TYPE_INLINE,)
+            return True, None
 
     def verify_external_module(self, repo_data: RepositoryData) -> [bool, str]:
-        from services.service_factory import SERVICE_FACTORY
-        repo_service_factory = SERVICE_FACTORY.get_external_repo_service_factory()
-        try:
-            repo_service = repo_service_factory.get_service(repo_data.get_repo_type())
-        except Exception as e:
-            return False, repr(e)
+        # lock is required here as temp directory for transformation may be written by multiple threads
+        with self._factory_lock:
+            from services.service_factory import SERVICE_FACTORY
+            repo_service_factory = SERVICE_FACTORY.get_external_repo_service_factory()
+            try:
+                repo_service = repo_service_factory.get_service(repo_data.get_repo_type())
+            except Exception as e:
+                return False, repr(e)
 
-        repo_path = repo_service.fetch_repository(repo_data)
-        external_module_service = SERVICE_FACTORY.get_external_module_service()
+            repo_path = repo_service.fetch_repository(repo_data)
+            external_module_service = SERVICE_FACTORY.get_external_module_service()
 
-        if repo_data.get_requirements_path():
-            requirements_path = os.path.join(repo_path, repo_data.get_requirements_path())
-            requirements_path = os.path.normpath(requirements_path)
-            success, error = external_module_service.install_requirements(requirements_path)
+            if repo_data.get_requirements_path():
+                requirements_path = os.path.join(repo_path, repo_data.get_requirements_path())
+                requirements_path = os.path.normpath(requirements_path)
+                success, error = external_module_service.install_requirements(requirements_path)
+                if not success:
+                    return False, f'Error occurred while installing requirements: {error}'
+
+            module_path = os.path.join(repo_path, repo_data.get_module_path())
+            module_path = os.path.normpath(module_path)
+            success, error, module = external_module_service.import_module(repo_data.get_module_path(), module_path)
             if not success:
-                return False, f'Error occurred while installing requirements: {error}'
+                return False, f'Error occurred while importing module: {error}'
 
-        module_path = os.path.join(repo_path, repo_data.get_module_path())
-        module_path = os.path.normpath(module_path)
-        success, error, module = external_module_service.import_module(repo_data.get_module_path(), module_path)
-        if not success:
-            return False, f'Error occurred while importing module: {error}'
+            success, error, _ = external_module_service.load_transformation(module, repo_data.get_transformation_name())
+            if not success:
+                return False, f'Error occurred while loading transformation: {error}'
 
-        success, error, _ = external_module_service.load_transformation(module, repo_data.get_transformation_name())
-        if not success:
-            return False, f'Error occurred while loading transformation: {error}'
-
-        try:
-            repo_service.clean_up_repository(repo_path)
-        except Exception:
-            pass
-        return True, None
+            try:
+                repo_service.clean_up_repository(repo_path)
+            except Exception:
+                pass
+            return True, None
 
     def load_external_module(self, transformation_id: str, repo_data: RepositoryData) -> [bool, str]:
-        from services.service_factory import SERVICE_FACTORY
-        repo_service_factory = SERVICE_FACTORY.get_external_repo_service_factory()
-        try:
-            repo_service = repo_service_factory.get_service(repo_data.get_repo_type())
-        except Exception as e:
-            return False, repr(e)
+        with self._factory_lock:
+            if transformation_id in self._transformation_map:
+                transformation = self._transformation_map.pop(transformation_id)
+                del transformation
 
-        repo_path = repo_service.fetch_repository(repo_data)
-        external_module_service = SERVICE_FACTORY.get_external_module_service()
+            from services.service_factory import SERVICE_FACTORY
+            repo_service_factory = SERVICE_FACTORY.get_external_repo_service_factory()
+            try:
+                repo_service = repo_service_factory.get_service(repo_data.get_repo_type())
+            except Exception as e:
+                return False, repr(e)
 
-        if repo_data.get_requirements_path():
-            requirements_path = os.path.join(repo_path, repo_data.get_requirements_path())
-            requirements_path = os.path.normpath(requirements_path)
-            success, error = external_module_service.install_requirements(requirements_path)
+            repo_path = repo_service.fetch_repository(repo_data)
+            external_module_service = SERVICE_FACTORY.get_external_module_service()
+
+            if repo_data.get_requirements_path():
+                requirements_path = os.path.join(repo_path, repo_data.get_requirements_path())
+                requirements_path = os.path.normpath(requirements_path)
+                success, error = external_module_service.install_requirements(requirements_path)
+                if not success:
+                    return False, f'Error occurred while installing requirements: {error}'
+
+            module_path = os.path.join(repo_path, repo_data.get_module_path())
+            module_path = os.path.normpath(module_path)
+            success, error, module = external_module_service.import_module(module_path)
             if not success:
-                return False, f'Error occurred while installing requirements: {error}'
+                return False, f'Error occurred while importing module: {error}'
 
-        module_path = os.path.join(repo_path, repo_data.get_module_path())
-        module_path = os.path.normpath(module_path)
-        success, error, module = external_module_service.import_module(module_path)
-        if not success:
-            return False, f'Error occurred while importing module: {error}'
+            success, error, transformation = \
+                external_module_service.load_transformation(module, repo_data.get_transformation_name())
+            if not success:
+                return False, f'Error occurred while loading transformation: {error}'
 
-        success, error, transformation = \
-            external_module_service.load_transformation(module, repo_data.get_transformation_name())
-        if not success:
-            return False, f'Error occurred while loading transformation: {error}'
-
-        self._transformation_map[transformation_id] = (transformation, EXECUTABLE_TYPE_CALLABLE,)
-        return True, None
+            self._transformation_map[transformation_id] = (transformation, EXECUTABLE_TYPE_CALLABLE,)
+            return True, None
 
     def get_transformations(self, transformation_ids: List[str]) -> Tuple[List[TransformationExecutable], List[str]]:
         transformations = []
         missing_ids = []
-        for transformation_id in transformation_ids:
-            if transformation_id not in self._transformation_map:
-                missing_ids.append(transformation_id)
-            else:
-                transformation, exec_type = self._transformation_map.get(transformation_id)
-                transformations.append(TransformationExecutable(transformation_id, transformation, exec_type))
+        with self._factory_lock:
+            for transformation_id in transformation_ids:
+                if transformation_id not in self._transformation_map:
+                    missing_ids.append(transformation_id)
+                else:
+                    transformation, exec_type = self._transformation_map.get(transformation_id)
+                    transformations.append(TransformationExecutable(transformation_id, transformation, exec_type))
 
-        return transformations, missing_ids
+            return transformations, missing_ids
